0d0a56fa6abe615e1ee9a49ae54ddcbcaefe2f2f
Pablo Marin <83443666+pmarin2592@users.noreply.github.com>
2025-04-23 15:13:20 -0600

    carga

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import sys
 from PIL import Image
 from datos.GestorDatos import GestorDatos
+from streamlit_modal import Modal
 
 
 # Añadir la raíz del proyecto al path
@@ -53,3 +54,47 @@ elif opcion == "Mapas Interactivos":
     elif submenu == "Accidentes con Víctimas":
         visualizador.carga_mapa_accidentes()
 
+# Crear instancia del modal
+modal = Modal("ℹ️ Acerca de esta Aplicación", key="acerca_modal", max_width=700)
+
+# Botón en el menú lateral
+with st.sidebar:
+    if st.button("ℹ️ Acerca de"):
+        modal.open()
+
+# Mostrar el modal si está abierto
+if modal.is_open():
+    with modal.container():
+        st.markdown("""
+        Esta aplicación es una demostración del trabajo realizado por estudiantes del curso **BD-143 Programación II** del  
+        **Diplomado en Big Data** del **Colegio Universitario de Cartago (CUC)** durante el **I Cuatrimestre 2025**,  
+        como parte de su aprendizaje y desarrollo de habilidades en el campo del análisis de datos a gran escala.
+        """)
+
+        st.markdown("### 👩‍💻 Autores:")
+        st.markdown("""
+        - Nubia Brenes Valerín  
+        - Pablo Marín Castillo  
+        - Eduardo Núñez Morales
+        """)
+
+        st.markdown("### 🛠️ Herramientas y Tecnologías Utilizadas:")
+        st.markdown("""
+        - **Lenguaje de Programación:** Python  
+        - **Librerías de Análisis de Datos:** Pandas, NumPy  
+        - **Visualización de Datos:** Matplotlib, Seaborn, Plotly  
+        - **Framework/Entorno:** Streamlit  
+        - **Base de Datos:** PostgreSQL  
+        - **Otras Herramientas:** Jupyter Notebooks, Microsoft Azure
+        """)
+
+st.sidebar.markdown(
+    """
+    <hr style='margin-top: 50px;'>
+    <div style='font-size: 0.8em; text-align: center;'>
+        © 2025 | Proyecto Final Programación II<br>
+        Big Data CUC
+    </div>
+    """,
+    unsafe_allow_html=True
+)
